05918933bdd31c5e561a214560e312f63c466c8a
ZeroVoid <joefei@qq.com>
2018-08-14 22:02:55 +0800

    导入IMKit和IMLib库

---
+++ app/src/main/java/com/fjycnet/rongclouddemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fjycnet.rongclouddemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.fjycnet.main.R.layout.activity_main);
+
+    }
+}
